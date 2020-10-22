30c0e3f87b22bb0664f4e36f5b7fb281f3b4a825
Mark Waite <mark.earl.waite@gmail.com>
2020-10-21 19:36:12 -0600

    Tidy the Javascript

--- main.js
+++ main.js
@@ -9,90 +9,93 @@ var caughtPokemons = 0;
 var message = null;
 
 // See https://www.pokemon.com/us/pokedex/ for the association of Pokemon to number
-var pokemon_names_and_images = { 'bulbasaur' : 1, 'ivysaur' : 2, 'venusaur' : 3, 'charmander' : 4, 'charmeleon' : 5,
-                                 'squirtle' : 7, 'wartortle' : 8, 'metapod' : 11, 'butterfree' : 12 };
+var pokemon_names_and_images = {
+  'bulbasaur': 1,
+  'ivysaur': 2,
+  'venusaur': 3,
+  'charmander': 4,
+  'charmeleon': 5,
+  'squirtle': 7,
+  'wartortle': 8,
+  'metapod': 11,
+  'butterfree': 12
+};
 
 // Create our 'main' state that will contain the game
 var mainState = {
 
-    preload: function() {
-	game.load.crossOrigin = 'anonymous';
+  preload: function() {
+    game.load.crossOrigin = 'anonymous';
 
-	// Load a background image
-	game.load.image('background', 'https://source.unsplash.com/1024x600/?colorado');
+    // Load a background image
+    game.load.image('background', 'https://source.unsplash.com/1024x600/?colorado');
 
-	// See https://www.pokemon.com/us/pokedex/ for the association of Pokemon to number
-	game.load.image('pokemon', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'); // charmander
-	game.load.image('bulbasaur', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png');
-	game.load.image('ivysaur', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png');
-	game.load.image('venusaur', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png');
-	game.load.image('charmander', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png');
-	game.load.image('charmeleon', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png');
-	game.load.image('squirtle', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png');
-	game.load.image('wartortle', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png');
-	game.load.image('metapod', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png');
-	game.load.image('butterfree', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png');
-   },
-
-    create: function() {
-
-      	// Set the background image
-    	game.add.tileSprite(0, 0, screenWidth, screenHeight, 'background');
-    	// Set the physics system
-     	game.physics.startSystem(Phaser.Physics.ARCADE);
-
-     	// Create text container and set it to initial value
-     	message = game.add.text(0, 0, 'Catch a Pokemon by clicking on it');
-	// message.fill = '#FF0000';
-    },
-
-    update: function() {
-	updatesCounter++;
-	// Create pokemon only every 100th call of update function
-        if(updatesCounter % 100 == 0) {
-	    createPokemon();
-	}
-	message.text = 'Congratulations, you caught ' + caughtPokemons; // + ' Pokemon. Score: ' + score;
+    // See https://www.pokemon.com/us/pokedex/ for the association of Pokemon to number
+    var pokedexURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+    for (var pokemonName in pokemon_names_and_images) {
+      game.load.image(pokemonName, pokedexURL + pokemon_names_and_images[pokemonName] + '.png');
     }
+  },
+
+  create: function() {
+
+    // Set the background image
+    game.add.tileSprite(0, 0, screenWidth, screenHeight, 'background');
+    // Set the physics system
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    // Create text container and set it to initial value
+    message = game.add.text(0, 0, 'Catch a Pokemon by clicking on it');
+    // message.fill = '#FF0000';
+  },
+
+  update: function() {
+    updatesCounter++;
+    // Create pokemon only every 100th call of update function
+    if (updatesCounter % 100 == 0) {
+      createPokemon();
+    }
+    message.text = 'Congratulations, you caught ' + caughtPokemons; // + ' Pokemon. Score: ' + score;
+  }
 
 };
 
 function createPokemon() {
-	// Select initial x position of the pokemon randomly
- 	var horizontalPosition = parseInt(screenWidth * Math.random());
- 	var verticalPosition = 0;
+  // Select initial x position of the pokemon randomly
+  var horizontalPosition = parseInt(screenWidth * Math.random());
+  var verticalPosition = 0;
 
-	// We already have several pre-defined shapes.
-	// 'bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'squirtle', 'wartortle', 'metapod', 'butterfree'
+  // We already have several pre-defined shapes.
+  // 'bulbasaur', 'ivysaur', 'venusaur', 'charmander', 'charmeleon', 'squirtle', 'wartortle', 'metapod', 'butterfree'
 
-	// Display the Pokemon on the screen
-	var pokemon = game.add.sprite(horizontalPosition, verticalPosition, 'charmander');
+  // Display the Pokemon on the screen
+  var pokemon = game.add.sprite(horizontalPosition, verticalPosition, 'charmander');
 
-	// Enables all kind of input actions on this image (click, etc)
-	pokemon.inputEnabled = true;
+  // Enables all kind of input actions on this image (click, etc)
+  pokemon.inputEnabled = true;
 
-	// Do stuff when the mouse is clicked over the Pokemon
-	pokemon.events.onInputDown.add(function(){
-	    caughtPokemons++;
-	    // score += 100;
-	    // score += Math.round(pokemon.body.speed);
-	    pokemon.destroy();
-	}, this);
+  // Do stuff when the mouse is clicked over the Pokemon
+  pokemon.events.onInputDown.add(function() {
+    caughtPokemons++;
+    // score += 100;
+    // score += Math.round(pokemon.body.speed);
+    pokemon.destroy();
+  }, this);
 
-	// Do stuff when the cursor is hovering over the Pokemon
-	pokemon.events.onInputOver.add(function() {
+  // Do stuff when the cursor is hovering over the Pokemon
+  pokemon.events.onInputOver.add(function() {
 
-	}, this);
+  }, this);
 
-	pokemon.checkWorldBounds = true;
-	pokemon.events.onOutOfBounds.add(function() {
-		// score = Math.max(0, score-100);
-		pokemon.destroy();
-	}, this);
+  pokemon.checkWorldBounds = true;
+  pokemon.events.onOutOfBounds.add(function() {
+    // score = Math.max(0, score-100);
+    pokemon.destroy();
+  }, this);
 
-	// Add gravity to the pokemon to make it fall
-	game.physics.arcade.enable(pokemon);
-	pokemon.body.gravity.y = 300;
+  // Add gravity to the pokemon to make it fall
+  game.physics.arcade.enable(pokemon);
+  pokemon.body.gravity.y = 300;
 };
 
 // Add and start the 'main' state to start the game
